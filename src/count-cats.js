cff82cd7a39e60910b451f682aa6723312fcf9d9
Konrad <fprognoz@gmail.com>
2023-04-14 03:42:29 +0300

    count-cats is solved

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let cats = 0;
+  matrix.forEach((array) => {
+    array.forEach((element) => {
+      if (element === "^^") {
+        cats++;
+      }
+    });
+  });
+  return cats;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
