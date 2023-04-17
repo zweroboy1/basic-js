d17445fcdd589b76a2ca942b23a34fb14f80c430
Konrad <fprognoz@gmail.com>
2023-04-17 16:16:35 +0300

    feat: solve dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let stats = {};
+  domains.forEach((domain) => {
+    let parts = domain.split(".");
+    let prev = "";
+    for (let i = parts.length - 1; i >= 0; i--) {
+      prev += "." + parts[i];
+      stats[prev] = stats[prev] + 1 || 1;
+    }
+  });
+  return stats;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
