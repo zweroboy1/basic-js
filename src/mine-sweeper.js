e6aa9f42273bd709aca1cb7cd3fcf0ae96259d32
Konrad <fprognoz@gmail.com>
2023-04-17 10:23:30 +0300

    feat: solve mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [];
+  let correctIndex = (a, b) => !(matrix[a] === undefined || matrix[a][b] === undefined);
+  for (let i = 0; i < matrix.length; i++) {
+    result.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      let bombs = 0;
+      let neighbors = [
+        [i - 1, j - 1],
+        [i - 1, j],
+        [i - 1, j + 1],
+        [i, j - 1],
+        [i, j + 1],
+        [i + 1, j - 1],
+        [i + 1, j],
+        [i + 1, j + 1],
+      ];
+      neighbors.forEach((arr) => {
+        if (correctIndex(arr[0], arr[1])) {
+          bombs += +matrix[arr[0]][arr[1]];
+        }
+      });
+      result[i][j] = bombs;
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
